b671c22a4b3fb251f226d4acde48f848ce29c340
Rok Nikolič <rok.nikolic@gmail.com>
2024-06-16 18:54:45 +0200

    added UI image

--- src/main/java/AMP/Player.java
+++ src/main/java/AMP/Player.java
@@ -2,7 +2,6 @@ package AMP;
 
 import processing.core.*;
 import ddf.minim.*;
-import ddf.minim.analysis.*;
 
 public class Player extends PApplet {
     Minim minim;
@@ -24,35 +23,15 @@ public class Player extends PApplet {
 
     @Override
     public void draw() {
-        // background(150); // comment za task 5
         float amount_played = (float) player.position() / player.length();
 
         float loudness = player.mix.level();
         float size = loudness * 100;
-        fill(204, 204, 255);
-        // Horizontal
-        //ellipse(amount_played * width, 500, size, size);
         float screen_width = (amount_played * width * 10) % width;
         float screen_height = (((amount_played * width * 10) / width) + 1) * 70 - 35;
-        // Whole screen
+
+        fill(204, 204, 255);
         ellipse(screen_width, screen_height, size, size);
-
-        float[] array = player.mix.toArray();
-        int len_of_array = array.length;
-        float column_width = (float) width / len_of_array;
-        for (int i = 0; i < len_of_array; i++) {
-            rect(column_width * i, 0, column_width, array[i] * height);
-        }
-
-        FFT fft = new FFT(len_of_array, player.sampleRate());
-        fft.forward(array);
-        float size2 = fft.specSize();
-        float column_width2 = (float) width / size2;
-        for (int i = 0; i < size2; i++) {
-            float value = fft.getBand(i);
-           rect(column_width2 * i, 0, column_width, value);
-        }
-
     }
 
     @Override
